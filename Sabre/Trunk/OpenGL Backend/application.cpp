fd3153601530d71f3db0a1e2ec140adbf8e22f14
Luís Reis <luiscubal@gmail.com>
2008-10-25 22:50:19 +0000

    Resizing now works correctly.

--- Sabre/Trunk/OpenGL Backend/application.cpp
+++ Sabre/Trunk/OpenGL Backend/application.cpp
@@ -47,6 +47,13 @@ void pineapple::std::Application::initialize(int* argc, char*** argv)
     
     currentScene = NULL;
     
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    
     _initialized = true;
 }
 
