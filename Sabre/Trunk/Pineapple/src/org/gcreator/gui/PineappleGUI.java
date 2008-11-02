4cc2122885496f3fcefbbb9ef5cec49f1e28ca7d
bobserge <bobserge@1b6dd1f4-0bef-4bc2-93b5-a664bc25159d>
2008-11-02 22:24:09 +0000

    Improved File/Tree stuff.

--- Sabre/Trunk/Pineapple/src/org/gcreator/gui/PineappleGUI.java
+++ Sabre/Trunk/Pineapple/src/org/gcreator/gui/PineappleGUI.java
@@ -26,6 +26,8 @@ package org.gcreator.gui;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -40,8 +42,10 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -73,7 +77,7 @@ import org.noos.xing.mydoggy.plaf.MyDoggyToolWindowManager;
  * @author Luís Reis
  */
 public class PineappleGUI implements EventHandler {
-    
+
     /**
      * The project tree
      */
@@ -111,7 +115,6 @@ public class PineappleGUI implements EventHandler {
     public static JMenuItem projectRemove;
     public static JMenuItem projectOpen;
     public static JMenuItem projectAdd;
-    
     /**
      * Provides a way to deal with multiple documents.
      */
@@ -120,11 +123,24 @@ public class PineappleGUI implements EventHandler {
      * The current project
      */
     public static Project project = null;
+    private MyDoggyToolWindowManager manager;
+    /**
+     * Event when a file is deleted from the filesystem.
+     */
+    public static final String FILE_DELETED = "file-deleted";
+    /**
+     * Event when a file is removed from the project.
+     */
+    public static final String FILE_REMOVED = "file-removed";
 
     /**
      * Created and initilizes a new Pineapple GUI.
      */
     public PineappleGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+        }
         initialize();
     }
 
@@ -132,20 +148,23 @@ public class PineappleGUI implements EventHandler {
      * Initilizes the Pineapple GUI.
      */
     protected void initialize() {
+        /* Core Events */
         EventManager.addEventHandler(this, DefaultEventTypes.APPLICATION_INITIALIZED, EventPriority.HIGH);
         EventManager.addEventHandler(this, DefaultEventTypes.WINDOW_CREATED, EventPriority.MEDIUM);
         EventManager.addEventHandler(this, DefaultEventTypes.WINDOW_DISPOSED, EventPriority.MEDIUM);
         EventManager.addEventHandler(this, DefaultEventTypes.FILE_OPENED, EventPriority.LOW);
-        EventManager.addEventHandler(this, DefaultEventTypes.FILE_CHANGED, EventPriority.MEDIUM);
-        EventManager.addEventHandler(this, DefaultEventTypes.PROJECT_OPENED, EventPriority.MEDIUM);
+        
+        /* Custom Events */
+        EventManager.addEventHandler(this, FILE_DELETED, EventPriority.LOW);
+        EventManager.addEventHandler(this, FILE_REMOVED, EventPriority.LOW);
     }
-    private MyDoggyToolWindowManager manager;
 
     /**
      * Initilize's the Pineapple Window.
      */
     protected void initializeWindow() {
         MainFrame f = Core.getStaticContext().getMainFrame();
+        SwingUtilities.updateComponentTreeUI(f);
         f.setTitle("Pineapple IDE");
         f.setIconImage(new ImageIcon(getClass().getResource(
                 "/org/gcreator/pineapple/pineapple.png")).getImage());
@@ -164,19 +183,65 @@ public class PineappleGUI implements EventHandler {
 
             @Override
             public void mousePressed(MouseEvent e) {
-                Object o = tree.getPathForRow(tree.getClosestRowForLocation(e.getX(), e.getY())).getLastPathComponent();
-                if (o == null || !(o instanceof BaseTreeNode)) {
+                final Object o = tree.getPathForRow(tree.getClosestRowForLocation(e.getX(), e.getY())).getLastPathComponent();
+                if (o == null) {
                     return;
                 }
-                BaseTreeNode node = (BaseTreeNode) o;
-
-                if (e.getClickCount() >= 2) {
-                   
+                if (e.getClickCount() >= 2 && e.getButton() == MouseEvent.BUTTON1 && o instanceof BaseTreeNode) {
+                    BaseTreeNode node = (BaseTreeNode) o;
                     TreePath tp = tree.getClosestPathForLocation(e.getX(), e.getY());
                     tree.setSelectionPath(tp);
 
                     if (node != null && node instanceof FileTreeNode) {
                         openFile(node.getElement().getFile());
+                    }
+                } else if (e.getClickCount() == 1 && e.getButton() == MouseEvent.BUTTON3) {
+                    JPopupMenu menu = new JPopupMenu("Tree");
+                    if (o instanceof FileTreeNode) {
+                        menu.add("Open...").addActionListener(new ActionListener() {
+
+                            public void actionPerformed(ActionEvent e) {
+                                openFile(((FileTreeNode) o).getElement().getFile());
+                            }
+                        });
+                    }
+                    
+                    if (o instanceof BaseTreeNode && project.getFiles().indexOf(((BaseTreeNode)o).getElement()) != -1) {
+                        final BaseTreeNode t = (BaseTreeNode) o;
+                        menu.add("Remove").addActionListener(new ActionListener() {
+
+                            public void actionPerformed(ActionEvent e) {
+                                project.remove(t.getElement());
+                                EventManager.fireEvent(this, FILE_REMOVED, t.getElement());
+                            }
+                        });
+                    }
+                    
+                    if (menu.getComponentCount() > 0) {
+                        menu.show(tree, e.getX(), e.getY());
+                    }
+                }
+            }
+        });
+        tree.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                Object o = tree.getSelectionPath().getLastPathComponent();
+                if (o == null || !(o instanceof FileTreeNode)) {
+                    return;
+                }
+                FileTreeNode t = (FileTreeNode) o;
+                BaseElement el = t.getElement();
+
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    openFile(el.getFile());
+                } else if (e.getKeyCode() == KeyEvent.VK_DELETE) {
+                    if (JOptionPane.showConfirmDialog(tree,
+                            "<html>Are you sure you want to delete this file from your file system?<br/>This will be permanent.</html>") == JOptionPane.YES_OPTION) {
+                        t.getElement().getFile().delete();
+                        project.remove(el);
+                        EventManager.fireEvent(this, FILE_DELETED, el);
                     }
                 }
             }
@@ -191,10 +256,9 @@ public class PineappleGUI implements EventHandler {
                     return;
                 }
                 BaseTreeNode node = (BaseTreeNode) o;
-                
+
                 projectRemove.setEnabled(project.getFiles().indexOf(node.getElement()) != -1);
                 projectOpen.setEnabled(node instanceof FileTreeNode);
-                e.getNewLeadSelectionPath();
             }
         });
         tree.setScrollsOnExpand(true);
@@ -272,7 +336,7 @@ public class PineappleGUI implements EventHandler {
         fileMenu.add(fileExit);
 
         projectMenu = new JMenu("Project");
-        
+
         projectOpen = new JMenuItem("Open Selected...");
         projectOpen.setMnemonic('O');
         projectOpen.setVisible(true);
@@ -283,11 +347,12 @@ public class PineappleGUI implements EventHandler {
                 try {
                     BaseTreeNode node = (BaseTreeNode) tree.getSelectionPath().getLastPathComponent();
                     openFile(node.getElement().getFile());
-                } catch (Exception e) {}
+                } catch (Exception e) {
+                }
             }
         });
         projectMenu.add(projectOpen);
-        
+
         projectAdd = new JMenuItem("Add File/Folder...");
         projectAdd.setMnemonic('A');
         projectAdd.setVisible(true);
@@ -298,7 +363,7 @@ public class PineappleGUI implements EventHandler {
             }
         });
         projectMenu.add(projectAdd);
-        
+
         projectRemove = new JMenuItem("Remove Selected");
         projectRemove.setMnemonic('R');
         projectRemove.setVisible(true);
@@ -306,14 +371,15 @@ public class PineappleGUI implements EventHandler {
         projectRemove.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent evt) {
-                project.remove(((BaseTreeNode)tree.getSelectionPath().getLastPathComponent()).getElement());
-                tree.updateUI();
+                BaseElement el = ((BaseTreeNode) tree.getSelectionPath().getLastPathComponent()).getElement();
+                project.remove(el);
+                EventManager.fireEvent(this, FILE_REMOVED, el);
             }
         });
         projectMenu.add(projectRemove);
-        
+
         menubar.add(projectMenu);
-        
+
         editMenu = new JMenu("Edit");
         editMenu.setMnemonic('E');
         editMenu.setEnabled(false);
@@ -357,24 +423,22 @@ public class PineappleGUI implements EventHandler {
      */
     @Override
     public void handleEvent(NotifyEvent evt) {
-        if(evt.getEventType().equals(DefaultEventTypes.APPLICATION_INITIALIZED)) {
+        if (evt.getEventType().equals(DefaultEventTypes.APPLICATION_INITIALIZED)) {
             boolean lf = false;
-            try{
+            try {
                 UIManager.setLookAndFeel(
                         "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
                 lf = true;
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Failed to install l&f: " + e.getMessage());
             }
-            if(!lf){
-                try{
+            if (!lf) {
+                try {
                     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e) {
                 }
-                catch(Exception e){}
             }
-        }
-        else if (evt.getEventType().equals(DefaultEventTypes.WINDOW_CREATED)) {
+        } else if (evt.getEventType().equals(DefaultEventTypes.WINDOW_CREATED)) {
 
             /* Initilize the main window */
             initializeWindow();
@@ -433,8 +497,18 @@ public class PineappleGUI implements EventHandler {
                 }
             }
 
-        } else if (evt.getEventType().equals(DefaultEventTypes.PROJECT_OPENED)) {
-            //TODO: open project.
+        } else if (evt.getEventType().equals(DefaultEventTypes.PROJECT_OPENED)) {            //TODO: open project.
+        } else if (evt.getEventType().equals(FILE_DELETED)) {
+            if (evt.getArguments().length > 0 && evt.getArguments()[0] instanceof BaseElement) {
+                BaseElement e = (BaseElement) evt.getArguments()[0];
+                if (e.getParent() != null && e.getParent() instanceof FolderElement) {
+                    ((FolderElement) e.getParent()).reload();
+                }
+                tree.updateUI();
+            }
+        } else if (evt.getEventType().equals(FILE_REMOVED)) {
+
+            tree.updateUI();
         }
     }
 
