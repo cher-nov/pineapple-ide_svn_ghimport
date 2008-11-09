15c0f9cf4857bee3a35d63ce036d760a3bfb8514
y0dud3 <y0dud3@1b6dd1f4-0bef-4bc2-93b5-a664bc25159d>
2008-11-09 03:15:34 +0000

    gravity, default actor/scene update/draw

--- Trunk/Sabre/OpenGL Backend/scene.cpp
+++ Trunk/Sabre/OpenGL Backend/scene.cpp
@@ -2,8 +2,17 @@
 
 using namespace SDLEngine;
 
+void Scene::update()
+{
+    for (int i = 0; i < actors.size(); i++)
+    {
+        actors[i]->update();
+    }
+}
+
 void Scene::draw()
 {
+    glClear(GL_COLOR_BUFFER_BIT);
     for (int i = 0; i < actors.size(); i++)
     {
         actors[i]->draw();
