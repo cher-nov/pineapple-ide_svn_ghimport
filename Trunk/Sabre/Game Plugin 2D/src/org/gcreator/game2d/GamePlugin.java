61655ad4a896981a47983e6ba1426c9628744565
luiscubal <luiscubal@1b6dd1f4-0bef-4bc2-93b5-a664bc25159d>
2008-11-25 15:20:27 +0000

    Game Plugin 2D: Started Action List.

--- Trunk/Sabre/Game Plugin 2D/src/org/gcreator/game2d/GamePlugin.java
+++ Trunk/Sabre/Game Plugin 2D/src/org/gcreator/game2d/GamePlugin.java
@@ -22,10 +22,20 @@ THE SOFTWARE.
  */
 package org.gcreator.game2d;
 
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.gcreator.gui.DocumentPane;
+import org.gcreator.gui.PineappleGUI;
+import org.gcreator.gui.TestActionRenderer;
 import org.gcreator.managers.EventManager;
 import org.gcreator.pineapple.PineappleCore;
+import org.gcreator.plugins.DefaultEventTypes;
 import org.gcreator.plugins.Event;
+import org.gcreator.plugins.EventPriority;
 import org.gcreator.plugins.Plugin;
+import org.noos.xing.mydoggy.ToolWindow;
+import org.noos.xing.mydoggy.ToolWindowAnchor;
 
 /**
  * The Game 2D plugin
@@ -34,6 +44,11 @@ import org.gcreator.plugins.Plugin;
  */
 public class GamePlugin extends Plugin {
 
+    public static final String PALETTE_CREATED = "game-palette-created";
+    
+    public static ToolWindow palette = null;
+    public static JPanel palettePanel = null;
+    
     @Override
     public String getName() {
         return "Game Plugin 2D";
@@ -46,7 +61,31 @@ public class GamePlugin extends Plugin {
 
     @Override
     public void handleEvent(Event e) {
-        if (e.getEventType().equals(PineappleCore.REGISTER_PROJECT_TYPES)) {
+        if (e.getEventType().equals(DefaultEventTypes.WINDOW_CREATED)) {
+            palettePanel = new JPanel();
+            palette = PineappleGUI.manager.registerToolWindow("Palette", "Palette", null, palettePanel,
+                ToolWindowAnchor.RIGHT);
+            palette.setAvailable(false);
+            EventManager.fireEvent(this, PALETTE_CREATED, palette, palettePanel);
+            
+            JFrame f = new JFrame();
+            f.setLayout(new BorderLayout());
+            TestActionRenderer act = new TestActionRenderer();
+            act.setVisible(true);
+            act.updateUI();
+            f.add(act, BorderLayout.CENTER);
+            f.setSize(300, 300);
+            f.setVisible(true);
+        }
+        else if (e.getEventType().equals(PineappleGUI.FILE_CHANGED)) {
+            DocumentPane p = PineappleGUI.dip.getSelectedDocument();
+            if(p!=null&&p instanceof PaletteUser){
+                palette.setAvailable(((PaletteUser) p).doPalette(palette, palettePanel));
+            }
+            else{
+                palette.setAvailable(false);
+            }
+        } else if (e.getEventType().equals(PineappleCore.REGISTER_PROJECT_TYPES)) {
             PineappleCore.addProjectType(new GameProjectType());
         }
     }
@@ -57,6 +96,8 @@ public class GamePlugin extends Plugin {
      */
     @Override
     public void initialize() {
+        EventManager.addEventHandler(this, DefaultEventTypes.WINDOW_CREATED, EventPriority.LOW);
+        EventManager.addEventHandler(this, PineappleGUI.FILE_CHANGED);
         EventManager.addEventHandler(this, PineappleCore.REGISTER_PROJECT_TYPES);
     }
 
